f7ce681fbe0ec0bf347323d8f6de775940062744
Gennaro Avolio-Toly <gennaro.avolio-toly@smashingideas.com>
2016-02-12 15:01:13 -0800

    setting up lanes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,33 +1,28 @@
 import AltContainer from 'alt-container';
 import React from 'react';
-import Notes from './Notes.jsx';
-import NoteActions from '../actions/NoteActions';
-import NoteStore from '../stores/NoteStore';
+import Lanes from './Lanes.jsx';
+import LaneActions from '../actions/LaneActions';
+import LaneStore from '../stores/LaneStore';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <button className="add-note" onClick={this.addNote}>+</button>
-        <AltContainer
-          stores={[NoteStore]}
-          inject={{
-            notes: () => NoteStore.getState().notes
-          }}
-        >
-          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
-        </AltContainer>
+        <button className="add-lane" onClick={this.addLane}>+</button>
+
+      <AltContainer
+        stores={[LaneStore]}
+        inject={{
+          lanes: () => LaneStore.getState().lanes || []
+        }}
+      >
+        <Lanes />
+      </AltContainer>
       </div>
     );
   }
 
-  addNote() {
-    NoteActions.create({task: 'New Task'});
-  }
-  editNote(id, task) {
-    NoteActions.update({id, task});
-  }
-  deleteNote(id) {
-    NoteActions.delete(id);
+  addLane() {
+    LaneActions.create({name: 'New lane'});
   }
 }
