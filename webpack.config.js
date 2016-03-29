e9a8b2673adee7a557d693713bb923ee70905409
Gennaro Avolio-Toly <gennaro.avolio-toly@smashingideas.com>
2016-03-29 11:09:26 -0700

    Split assets. Finish and deploy

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const HtmlwebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const NpmInstallPlugin = require('npm-install-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const pkg = require('./package.json');
 
 var stylelint = require('stylelint');
 var configSuitcss = require('stylelint-config-suitcss');
@@ -10,19 +13,24 @@ var configSuitcss = require('stylelint-config-suitcss');
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+  build: path.join(__dirname, 'build'),
+  style: path.join(__dirname, 'app/main.css')
 };
 
 process.env.BABEL_ENV = TARGET;
 
 const common = {
-  entry: PATHS.app,
+  entry: {
+    app: PATHS.app,
+    style: PATHS.style
+  },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   output: {
     path: PATHS.build,
-    filename: 'bundle.js'
+    filename: '[name].js'
+    // filename: 'bundle.js'
   },
   module: {
     preLoaders: [
@@ -38,11 +46,6 @@ const common = {
       }
     ],
     loaders: [
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-        include: PATHS.app
-      },
       {
         test: /\.jsx?$/,
         loaders: ['babel?cacheDirectory'],
@@ -60,9 +63,10 @@ const common = {
   },
   plugins: [
     new HtmlwebpackPlugin({
-      template: 'node_modules/html-webpack-template/index.html',
+      template: 'node_modules/html-webpack-template/index.ejs',
       title: 'Kanban app',
-      appMountId: 'app'
+      appMountId: 'app',
+      inject: false
     })
   ]
 };
@@ -84,6 +88,13 @@ if(TARGET === 'start' || !TARGET) {
       host: process.env.HOST,
       port: process.env.PORT
     },
+    module: {
+      loaders: [{
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      }]
+    },
     plugins: [
       new webpack.HotModuleReplacementPlugin(),
       new NpmInstallPlugin({
@@ -93,9 +104,33 @@ if(TARGET === 'start' || !TARGET) {
   });
 }
 
-if(TARGET === 'build') {
+if(TARGET === 'build' || TARGET === 'stats') {
   module.exports = merge(common, {
+    entry: {
+      vendor: Object.keys(pkg.dependencies).filter(function(v) {
+        return v !== 'alt-utils';
+      })
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].[chunkhash].js',
+      chunkFilename: '[chunkhash].js'
+    },
+    module: {
+      loaders: [{
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style', 'css'),
+        include: PATHS.app
+      }]
+    },
     plugins: [
+      new CleanPlugin([PATHS.build], {
+        verbose: false
+      }),
+      new ExtractTextPlugin('[name].[chunkhash].css'),
+      new webpack.optimize.CommonsChunkPlugin({
+        names: ['vendor', 'manifest']
+      }),
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': '"production"'
       }),
